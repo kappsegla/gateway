747652f7347139704fed4784ccb3e0544389a3a8
Martin Blomberg <martin.blomberg@outlook.com>
2023-05-17 15:26:31 +0200

    Initial Commit

---
+++ src/main/java/com/example/gateway/GatewayConfig.java
@@ -0,0 +1,45 @@
+package com.example.gateway;
+
+import org.springframework.cloud.gateway.filter.factory.DedupeResponseHeaderGatewayFilterFactory.Strategy;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+
+    final AuthenticationFilter jwtAuthFilter;
+
+    public GatewayConfig(AuthenticationFilter jwtAuthFilter) {
+        this.jwtAuthFilter = jwtAuthFilter;
+    }
+
+    @Bean
+    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("user-service", r -> r.path("/users/**")
+                        .filters(f -> f
+                                        .filter(jwtAuthFilter)
+                                        .dedupeResponseHeader("Access-Control-Allow-Origin", String.valueOf(Strategy.RETAIN_FIRST))
+                                //.rewritePath("/users/(?<segment>.*)", "/users/${segment}")
+                                //.addRequestHeader("userID", "")
+                        ).uri("http://localhost:8002"))
+                .route("image-service", r -> r.path("/images")
+                        .filters(f -> f
+                                .filter(jwtAuthFilter)
+                                .dedupeResponseHeader("Access-Control-Allow-Origin", String.valueOf(Strategy.RETAIN_FIRST)))
+                        .uri("http://localhost:8001"))
+                .route("image-service", r -> r.path("/images/*")
+                        .filters(f -> f
+                                .dedupeResponseHeader("Access-Control-Allow-Origin", String.valueOf(Strategy.RETAIN_FIRST)))
+                        .uri("http://localhost:8001"))
+                .route("auth-service", r -> r.path("/auth")
+                        .filters(f -> f
+                                .filter(jwtAuthFilter)
+                                .dedupeResponseHeader("Access-Control-Allow-Origin", String.valueOf(Strategy.RETAIN_FIRST))
+                                .rewritePath("/auth", "/api/auth/login"))
+                        .uri("http://localhost:8003")).build();
+
+    }
+}
---
+++ src/main/java/com/example/gateway/JwtUtil.java
@@ -0,0 +1,35 @@
+package com.example.gateway;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @PostConstruct
+    public void init() {
+    }
+
+    public Claims getAllClaimsFromToken(String token) {
+        return Jwts.parserBuilder().setSigningKey(secret).build().parseClaimsJws(token).getBody();
+    }
+
+    private boolean isTokenExpired(String token) {
+        return this.getAllClaimsFromToken(token).getExpiration().before(new Date());
+    }
+
+    public boolean isInvalid(String token) {
+        return this.isTokenExpired(token);
+    }
+
+}
